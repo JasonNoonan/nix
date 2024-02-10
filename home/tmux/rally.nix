f5200d89eee7668f94faef3b71877befc4ca3461
Jason Noonan <jason.noonan@pdq.com>
2024-02-10 13:54:07 -0600

    fix rally script

--- home/tmux/rally.nix
+++ home/tmux/rally.nix
@@ -1,9 +1,13 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 pkgs.writeShellScriptBin "rally" ''
   set -eu
   TARGET=$(ls -d ~/workspace/* ~/* ~/.config/* | ${pkgs.fzf}/bin/fzf --header-first --header="Launch Project" --prompt="🗡️  " --preview '${pkgs.eza}/bin/eza --tree --icons --color=always --level 3 --git-ignore {}')
   NAME=$(basename $TARGET)
   SESSION_NAME=$(echo $NAME | tr [:lower:] [:upper:])
 
-  ${pkgs.smug}/bin/smug start $NAME --debug -a 2>/dev/null || ${pkgs.smug}/bin/smug start default name=$SESSION_NAME root=$TARGET --debug -a
+  if [[ -f "$HOME/.config/smug/$NAME.yml" ]]; then
+    ${pkgs.smug}/bin/smug start $NAME -a
+  else
+    ${pkgs.smug}/bin/smug start default name=$SESSION_NAME root=$TARGET -a
+  fi
 ''
