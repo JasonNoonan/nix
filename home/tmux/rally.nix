63882babe221fd9daf3eb9ebd3892e3380919dfa
Jason Noonan <jason.noonan@pdq.com>
2024-08-12 12:55:31 -0500

    remove etc path from rally locations

--- home/tmux/rally.nix
+++ home/tmux/rally.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 pkgs.writeShellScriptBin "rally" ''
   set -eu
-  TARGET=$(ls -d ~/workspace/* ~/* ~/.config/* /etc/* | ${pkgs.fzf}/bin/fzf --header-first --header="Launch Project" --prompt="🗡️  " --preview '${pkgs.eza}/bin/eza --tree --icons --color=always --level 3 --git-ignore {}')
+  TARGET=$(ls -d ~/workspace/* ~/* ~/.config/* | ${pkgs.fzf}/bin/fzf --header-first --header="Launch Project" --prompt="🗡️  " --preview '${pkgs.eza}/bin/eza --tree --icons --color=always --level 3 --git-ignore {}')
   NAME=$(basename $TARGET)
   SESSION_NAME=$(echo $NAME | tr [:lower:] [:upper:])
 
