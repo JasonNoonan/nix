ce1db8cfd70cb13404e3ed784a1f09abfbd8b383
Jason Noonan <jason.noonan@pdq.com>
2025-02-24 14:25:13 -0600

    disable copilot plugin

--- home/neovim/neovim/lua/plugins/copilot-cmp.lua
+++ home/neovim/neovim/lua/plugins/copilot-cmp.lua
@@ -1,31 +1,37 @@
+-- sneaky/lazy disable this plugin
+-- remove this block or set to false to load the plugin
+if true then
+	return {}
+end
+
 ---@type LazySpec
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-    },
-  },
-  { "zbirenbaum/copilot-cmp", opts = {}, dependencies = { "zbirenbaum/copilot.lua" } },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "zbirenbaum/copilot-cmp" },
-    opts = function(_, opts)
-      -- Inject copilot into cmp sources, with high priority
-      table.insert(opts.sources, 1, {
-        name = "copilot",
-        group_index = 1,
-        priority = 10000,
-      })
-    end,
-  },
-  {
-    "onsails/lspkind.nvim",
-    optional = true,
-    -- Adds icon for copilot using lspkind
-    opts = function(_, opts)
-      opts.symbol_map.Copilot = ""
-    end,
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		opts = {
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+		},
+	},
+	{ "zbirenbaum/copilot-cmp", opts = {}, dependencies = { "zbirenbaum/copilot.lua" } },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = { "zbirenbaum/copilot-cmp" },
+		opts = function(_, opts)
+			-- Inject copilot into cmp sources, with high priority
+			table.insert(opts.sources, 1, {
+				name = "copilot",
+				group_index = 1,
+				priority = 10000,
+			})
+		end,
+	},
+	{
+		"onsails/lspkind.nvim",
+		optional = true,
+		-- Adds icon for copilot using lspkind
+		opts = function(_, opts)
+			opts.symbol_map.Copilot = ""
+		end,
+	},
 }
