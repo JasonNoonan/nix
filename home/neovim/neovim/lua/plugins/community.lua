bf77ccc4f6a02a32f50682209bdec52619715eff
Jason Noonan <jason.noonan@pdq.com>
2025-02-25 06:50:13 -0600

    add astrocommunity packs

---
+++ home/neovim/neovim/lua/plugins/community.lua
@@ -0,0 +1,7 @@
+return {
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.typescript-all-in-one" },
+}
