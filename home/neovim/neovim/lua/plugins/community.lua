e4d4e34c5286a5540e9ee556c27b1850cbaf9c3a
Jason Noonan <jason.noonan@pdq.com>
2025-04-10 09:53:36 -0500

    remove elixir-ls

--- home/neovim/neovim/lua/plugins/community.lua
+++ home/neovim/neovim/lua/plugins/community.lua
@@ -1,7 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.motion.grapple-nvim" },
-  { import = "astrocommunity.pack.elixir-phoenix" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
