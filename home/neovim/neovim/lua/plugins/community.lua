9c1315d2823b35f1380b76ece6bbbd337193550b
Jason Noonan <jason.noonan@pdq.com>
2025-05-21 18:54:11 -0500

    add copilot config

--- home/neovim/neovim/lua/plugins/community.lua
+++ home/neovim/neovim/lua/plugins/community.lua
@@ -5,4 +5,6 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.completion.avante-nvim" },
 }
