beb6d7f778ca4c2d62e2d804b5d332e12f41a171
Jason Noonan <jason.noonan@pdq.com>
2025-07-14 22:07:22 -0500

    remove avante and copilot

--- home/neovim/neovim/lua/plugins/community.lua
+++ home/neovim/neovim/lua/plugins/community.lua
@@ -5,6 +5,4 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
-  { import = "astrocommunity.completion.copilot-lua" },
-  { import = "astrocommunity.completion.avante-nvim" },
 }
