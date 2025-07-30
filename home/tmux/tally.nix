f56a6bdbeba2a17fb730a13d58129f2f82e8e3bc
Jason Noonan <jason.noonan@pdq.com>
2025-07-29 19:49:58 -0500

    replace . with - in session name to fix tmux session error

--- home/tmux/tally.nix
+++ home/tmux/tally.nix
@@ -4,7 +4,7 @@ pkgs.writeShellScriptBin "tally" ''
 
   TARGET=$(ls -d ~/workspace/* ~/* ~/.config/* | ${pkgs.fzf}/bin/fzf --header-first --header="Launch Project" --prompt="🗡️  " --preview '${pkgs.eza}/bin/eza --tree --icons --color=always --level 3 --git-ignore {}')
   NAME=$(basename $TARGET)
-  SESSION_NAME=$(echo $NAME | tr [:lower:] [:upper:])
+  SESSION_NAME=$(echo $NAME | tr [:lower:] [:upper:] | sed 's/\./-/g')
   SESSION_EXISTS=$(tmux has-session -t "$SESSION_NAME" 2>/dev/null && echo "true" || echo "false")
   echo "Session $SESSION_NAME exists?: $SESSION_EXISTS"
   IN_TMUX=$([[ -n "$TMUX" ]] && echo "true" || echo "false")
